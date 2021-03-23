9052679efc96f36d2f11d5177cf4a511a935d13b
xinxyu <xin.yu55@gmail.com>
2021-03-22 23:43:17 -0400

    remove password field from user detail form

--- sysadmin/forms/user_detail.py
+++ sysadmin/forms/user_detail.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 class CustomEditUserForm(UserChangeForm):
     first_name = forms.CharField(max_length=30, help_text='Required.')
     last_name = forms.CharField(max_length=30, help_text='Required.')
-
+    password = None
     class Meta:
         model = User
         fields = ('username','first_name', 'last_name', 'email', 'is_active')

03d041adcac0c93fd8bf2b483833064f21a67fed
Paul Sotherland <psotherland@g.harvard.edu>
2021-03-27 23:11:20 -0500

    whoops

--- web_app/models/main.py
+++ web_app/models/main.py
@@ -1,11 +1,11 @@
 from django.db import models
 from django_cryptography.fields import encrypt
 
-from web_app.models import permissions
-from web_app.models.permissions import *
+from emis import permissions
+from emis.permissions import CustomPermissionModel
 
 # Create your models here.
 
-class TestScore(models.Model):
+class TestScore(CustomPermissionModel):
     sensitive_data = encrypt(models.CharField(max_length=50))
     non_sensitive_data = models.CharField(max_length=50)

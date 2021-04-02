606a50a5df2e2a3642d1b63c435e033a10c172af
Paul Sotherland <psotherland@g.harvard.edu>
2021-04-01 22:43:33 -0500

    re-adding __init__.py

---
+++ authentication/models/__init__.py
@@ -0,0 +1,14 @@
+from .activation import Activation
+from .permissions import (
+    SchoolAdministration,
+    RestrictedSchoolAdministration,
+    EarlyChildhood,
+    Principal,
+    Teaching,
+    DistrictAdministration,
+    SchoolSupervision,
+    StatisticsAndPlanning,
+    EvaluationAndAssessment,
+    SupportServices,
+    ExternalAssessment
+)

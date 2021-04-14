608c589eff4af0b7bcee8d0d2073285c99f59e7a
Andrew Rodriguez <andrewr@twitter.com>
2021-04-14 14:11:23 -0700

    Start of school application UI

--- school/views.py
+++ school/views.py
@@ -48,22 +48,158 @@ def index(request):
 
 def all_districts_view(request):
     # check permission
-    data = District.objects.values("district_code", "district_name")
-    print(data)
-    return render(request, "all_districts.html", {"data": data})
+    # data = District.objects.values("district_code", "district_name")
+    context = {
+        "data": [
+            {
+                "district_code": "d1",
+                "district_name": "District 1",
+                "school_count": 4,
+                "teacher_count": 22,
+                "student_count": 230,
+            },
+            {
+                "district_code": "d2",
+                "district_name": "District 2",
+                "school_count": 3,
+                "teacher_count": 40,
+                "student_count": 430,
+            },
+            {
+                "district_code": "d3",
+                "district_name": "District 3",
+                "school_count": 2,
+                "teacher_count": 22,
+                "student_count": 330,
+            },
+        ]
+    }
+    context = _add_side_navigation_context(context)
+    return render(request, "all_districts.html", context)
 
 
 def single_district_view(request, code):
     # check permission
-    district = District.objects.get(district_code=code)
-    data = {
-        "district": {
-            "district_name": district.district_name,
-            "district_code": district.district_code,
-        },
-        "schools": School.objects.filter(district_name=district),
+    # district = District.objects.get(district_code=code)
+    context = {
+        "district_code": "d1",
+        "district_name": "District 1",
+        "school_count": 4,
+        "teacher_count": 22,
+        "student_count": 230,
+        "schools": [
+            {
+                "school_code": "school1",
+                "school_name": "School 1",
+                "teacher_count": 10,
+                "student_count": 23,
+            },
+            {
+                "school_code": "school2",
+                "school_name": "School 2",
+                "teacher_count": 10,
+                "student_count": 23,
+            },
+            {
+                "school_code": "school3",
+                "school_name": "School 3",
+                "teacher_count": 10,
+                "student_count": 23,
+            },
+            {
+                "school_code": "school4",
+                "school_name": "School 4",
+                "teacher_count": 10,
+                "student_count": 23,
+            },
+        ],
     }
-    return render(request, "single_district.html", {"data": data})
+    context = _add_side_navigation_context(context)
+    return render(request, "single_district.html", context)
+
+
+def single_school_view(request, code):
+    context = {
+        "district_code": "d1",
+        "district_name": "District 1",
+        "school_name": "School 3",
+        "school_code": "school3",
+        "student_count": 230,
+        "teacher_count": 10,
+        "course_count": 34,
+        "enrollment": [
+            {"year": 2022, "student_count": 45},
+            {"year": 2023, "student_count": 56},
+            {"year": 2024, "student_count": 45},
+            {"year": 2025, "student_count": 90},
+        ],
+        "teachers": [
+            {"teacher_code": "teacher1", "name": "Andrew", "course_count": 2},
+            {"teacher_code": "teacher2", "name": "Paul", "course_count": 2},
+            {"teacher_code": "teacher3", "name": "Xin", "course_count": 2},
+            {"teacher_code": "teacher4", "name": "Mildren", "course_count": 2},
+            {"teacher_code": "teacher5", "name": "Anditya", "course_count": 2},
+            {"teacher_code": "teacher6", "name": "Younghan", "course_count": 2},
+        ],
+        "courses": [
+            {
+                "subject_group": "Science",
+                "subject": "Physics",
+                "student_count": 100,
+                "instances": 3,
+            },
+            {
+                "subject_group": "Science",
+                "subject": "Physics",
+                "student_count": 100,
+                "instances": 3,
+            },
+            {
+                "subject_group": "Science",
+                "subject": "Physics",
+                "student_count": 100,
+                "instances": 3,
+            },
+            {
+                "subject_group": "Science",
+                "subject": "Physics",
+                "student_count": 100,
+                "instances": 3,
+            },
+        ],
+    }
+    context = _add_side_navigation_context(context)
+    return render(request, "single_school.html", context)
+
+
+def _add_side_navigation_context(context):
+    context["side_nav"] = [
+        {
+            "district_code": "d1",
+            "district_name": "District 1",
+            "schools": [
+                {"school_code": "school1", "school_name": "School 1"},
+                {"school_code": "school2", "school_name": "School 2"},
+            ],
+        },
+        {
+            "district_code": "d2",
+            "district_name": "District 2",
+            "schools": [
+                {"school_code": "school3", "school_name": "School 3"},
+                {"school_code": "school4", "school_name": "School 4"},
+            ],
+        },
+        {
+            "district_code": "d3",
+            "district_name": "District 3",
+            "schools": [
+                {"school_code": "school5", "school_name": "School 5"},
+                {"school_code": "school6", "school_name": "School 6"},
+            ],
+        },
+    ]
+    return context
 
 
 def teachers(request):

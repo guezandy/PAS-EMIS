db5a6bdd7bae6c4207b6f7cce00aa116b1d4d202
Mildred Fakoya <luvlyayee@gmail.com>
2021-04-17 16:54:44 -0400

    code cleaning

--- historical_surveillance/models.py
+++ historical_surveillance/models.py
@@ -1,5 +1,10 @@
+import datetime
+
 from django.db import models
 from emis.permissions import CustomPermissionModel
+
+import authentication
+from emis import settings
 
 GRADE_CHOICES = [
     ("grade k", "Grade k"),
@@ -23,23 +28,47 @@ CATEGORY_CHOICES = [
     ("public secondary", "Public Secondary"),
     ("private primary", "Private Primary"),
     ("private secondary", "Private Secondary"),
-    ("other", "Other"),
+    ("special education", "Special Education"),
+    ("primary", "Primary"),
+    ("secondary", "Secondary"),
 ]
 
 SEX_CHOICES = [("male", "Male"), ("female", "Female")]
 
-
-# TO-DO
-# Enforce Foreign Key constraint on the schools and districts
+MANAGEMENT_CHOICES = [
+    ("ministry of education", "Ministry of Education"),
+    ("board/council", "Board / Council"),
+    ("others", "Others"),
+]
+OWNERSHIP_CHOICES = [
+    ("government", "Government"),
+    ("private", "Private"),
+    ("denominational", "Denominational"),
+]
+TYPE_OF_SCHOOL_CHOICES = [
+    ("hearing impaired", "Hearing Impaired"),
+    ("visually Impaired", "Visually Impaired"),
+    ("blind", "Blind"),
+    ("autistic", "Autistic"),
+    ("physically impaired", "Physically Impaired"),
+    ("multiple handicaps", "Multiple Handicaps"),
+    ("mentally challenged", "Mentally Challenged"),
+]
+PLAYING_FIELD_CHOICES = [
+    ("community owned", "Community Owned"),
+    ("school owned", "School Owned"),
+    ("other", "Other"),
+]
 
 
 class District(models.Model):
     objects = None
-    created_at = models.DateField(auto_now_add=True)
-    created_by = models.CharField(max_length=255)
+    # created_at = models.DateField(auto_now_add=True)
+    created_at = models.DateField()
+    created_by = models.CharField(max_length=255, blank=True)
     district_code = models.CharField(max_length=50, blank=True)
     district_name = models.CharField(max_length=50)
-    updated_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField()
     updated_by = models.CharField(max_length=255, blank=True)
 
     class Meta(CustomPermissionModel.Meta):
@@ -52,9 +81,9 @@ class District(models.Model):
 class School(models.Model):
     objects = None
     created_at = models.DateField(auto_now_add=True)
-    created_by = models.CharField(max_length=255)
+    created_by = models.CharField(max_length=255, blank=True)
     school_code = models.CharField(max_length=50, blank=True)
-    school_name = models.CharField(max_length=50)
+    school_name = models.CharField(max_length=100)
     district_name = models.ForeignKey(District, on_delete=models.CASCADE)
     category_of_school = models.CharField(max_length=50, choices=CATEGORY_CHOICES)
     updated_at = models.DateField(auto_now_add=True)
@@ -64,7 +93,7 @@ class School(models.Model):
         pass
 
     def __str__(self):
-        return self.school_name
+        return str(self.school_name)
 
 
 class AggregateEnrollment(models.Model):
@@ -77,8 +106,8 @@ class AggregateEnrollment(models.Model):
     district_of_school = models.ForeignKey(District, on_delete=models.CASCADE)
     capacity_of_school = models.IntegerField()
     total_enrollment = models.IntegerField()
-    minimum_age = models.IntegerField(blank=True, null=True)
-    maximum_age = models.IntegerField(blank=True, null=True)
+    minimum_age = models.IntegerField(blank=True)
+    maximum_age = models.IntegerField(blank=True)
     updated_at = models.DateField(auto_now_add=True)
     updated_by = models.CharField(max_length=255, blank=True)
 
@@ -86,7 +115,7 @@ class AggregateEnrollment(models.Model):
         pass
 
     def __str__(self):
-        return self.name_of_school
+        return str(self.name_of_school)
 
 
 class Enrollment(models.Model):
@@ -101,8 +130,8 @@ class Enrollment(models.Model):
     district = models.ForeignKey(District, on_delete=models.CASCADE)
     grade = models.CharField(max_length=20, choices=GRADE_CHOICES, default=None)
     enrollment = models.IntegerField(null=True, blank=True)
-    minimum_age = models.IntegerField(blank=True, null=True)
-    maximum_age = models.IntegerField(blank=True, null=True)
+    minimum_age = models.IntegerField(blank=True)
+    maximum_age = models.IntegerField(blank=True)
     sex = models.CharField(max_length=20, null=True, choices=SEX_CHOICES)
     updated_at = models.DateField(auto_now_add=True)
     updated_by = models.CharField(max_length=255, blank=True)

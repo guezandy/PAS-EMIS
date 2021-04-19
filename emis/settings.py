1a290438e46a6189c991d024287832254bb6ea9a
Andrew Rodriguez <andrewr@twitter.com>
2021-04-19 08:30:16 -0700

    Updates to merge main

--- emis/settings.py
+++ emis/settings.py
@@ -71,9 +71,7 @@ ROOT_URLCONF = "emis.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [
-            (os.path.join(BASE_DIR, "templates")),
-        ],
+        "DIRS": [(os.path.join(BASE_DIR, "templates")),],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -101,9 +99,7 @@ DATABASES = {
         "PASSWORD": env("DATABASE_PASSWORD"),
         "HOST": env("DATABASE_HOST"),
         "PORT": env("DATABASE_PORT"),
-        "TEST": {
-            "NAME": "test_lucia_education",
-        },
+        "TEST": {"NAME": "test_lucia_education",},
     }
 }
 
@@ -115,15 +111,9 @@ AUTH_PASSWORD_VALIDATORS = [
     {
         "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
-    {
-        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
-    },
-    {
-        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
-    },
-    {
-        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
-    },
+    {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",},
+    {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",},
+    {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",},
 ]
 
 
